91f1a83879a1ca4dc2ad4ed8679b0c8a55dbd486
Jc Dior <jessicaclairdior@gmail.com>
2020-09-21 23:54:56 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,26 @@
+class CashRegister
+  attr_reader :total, :discount
 
+def initialize(discount = 20)
+  @total = 0
+  @discount = discount
+end
+
+def total
+  puts self.total
+end
+
+def add_item(title, price)
+end
+
+def apply_discount
+
+end
+
+def items
+end
+
+def void_last_transaction
+end
+
+end
