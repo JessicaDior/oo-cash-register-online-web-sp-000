2710644fd24d66a91badf8bb8459872fda1423af
Jc Dior <jessicaclairdior@gmail.com>
2020-09-22 00:43:45 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -5,11 +5,13 @@ class CashRegister
 def initialize(discount = 0)
   @total = 0
   @discount = discount
+  @items = []
 end
 
 
 def add_item(title, price, quantity = 1)
   @total += price * quantity
+  @items << title
 end
 
 def apply_discount
@@ -19,11 +21,6 @@ def apply_discount
    else
      "There is no discount to apply."
    end
-end
-
-def items
-  @items = []
-
 end
 
 def void_last_transaction
