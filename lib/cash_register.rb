6792ec343c9527099abb83a2ce4f9bd1e2fb8751
Jc Dior <jessicaclairdior@gmail.com>
2020-09-22 00:47:03 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,11 +6,15 @@ class CashRegister
 def initialize(discount = 0)
   @total = 0
   @discount = discount
+  @items = []
 end
 
 
 def add_item(title, price, quantity = 1)
-  @total += price * quantity
+  self.total += price * quantity
+  quantity.times do 
+    items << title
+
 end
 
 def apply_discount
