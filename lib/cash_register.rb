d6cefd93c53b5e4be9c5ff2d3a3383f4afac1cec
Jc Dior <jessicaclairdior@gmail.com>
2020-09-22 00:00:24 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,8 +14,8 @@ end
 
 def apply_discount
 if @discount = 20
-    @total = @total - @discount * 10
-  return "After the discount, the total comes to $#{@total}."
+  then  @total = @total - @discount * 10
+   return "After the discount, the total comes to $#{@total}."
 elsif @discount = 0
   return "There is no discount to apply."
 end
